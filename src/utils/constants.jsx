5ac8c1fb7d0ebc7850427d6e2c5dc2d472ff5e6d
{gurnoor} <{singhgurnoor283@gmail.com}>
2025-07-23 16:38:55 +0530

    added

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -5,10 +5,10 @@ export const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer '+ process.env.REACT_APP_TMDB_KEY
+    Authorization: 'Bearer '+ process.env.VITE_TMDB_KEY
   }
 };
 
 export const IMG_CDN_URL="https://image.tmdb.org/t/p/w300"
 
-export const API_KEY= process.env.REACT_APP_API_KEY
+export const API_KEY= process.env.VITE_API_KEY
